23824e6a6994ee37e1f499197203ae29aa6c4f17
deepak203adgitm <73418732+deepak203adgitm@users.noreply.github.com>
2021-07-12 04:01:19 -0700

    api added

---
+++ src/API/secret.js
@@ -0,0 +1,7 @@
+export const API_KEY = "80d89bdfc99847dd570d5290e792eb63";
+export const API_URL = "https://api.themoviedb.org/3";
+
+//https://api.themoviedb.org/3/movie/550?api_key=80d89bdfc99847dd570d5290e792eb63
+
+export const BASE_URL = "https://image.tmdb.org/t/p/w500/"; 
+export const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
